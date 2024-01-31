60539c5854c3098167583bf6908a141c4e2333d4
jaune-attend64 <martel.jonathan64@gmail.com>
2024-01-31 09:15:58 +0100

    maj

--- main.js
+++ main.js
@@ -5,26 +5,36 @@ const gameScore = document.getElementById("gameScore");
 const paper = document.getElementById("paper");
 const rock = document.getElementById("rock");
 const scissors = document.getElementById("scissors");
+const well = document.getElementById("well");
 
 function runGame(userChoice) {
   const computerChoice = choices[Math.floor(Math.random() * choices.length)];
 
   switch(userChoice + '_' + computerChoice) {
     case 'papier_ciseaux':
-    case 'caillou_papier':
+    case 'caillou_puit':
     case 'ciseaux_caillou':
+    case 'ciseaux_puit':
+    case 'puit_papier':
+    case 'caillou_puit': 
       loses += 1;
       gameStatus.innerHTML = `H: ${userChoice} | IA: ${computerChoice} -> IA Wins`
       break;
     case 'papier_caillou':
+    case 'caillou_papier':
     case 'caillou_ciseaux':
     case 'ciseaux_papier':
+    case 'puit_caillou':
+    case 'puit_ciseaux': 
+    case 'papier_puit': 
+    case 'puit_papier' :
       wins += 1;
       gameStatus.innerHTML = `H: ${userChoice} | IA: ${computerChoice} -> H Wins`
       break;
     case 'papier_papier':
     case 'caillou_caillou':
     case 'ciseaux_ciseaux':
+    case 'puit_puit':
       gameStatus.innerHTML = `H: ${userChoice} | IA: ${computerChoice} -> Egalité`
       break;
   }
@@ -35,3 +45,4 @@ function runGame(userChoice) {
 paper.addEventListener("click", () => runGame("papier"));
 rock.addEventListener("click", () => runGame("caillou"));
 scissors.addEventListener("click", () => runGame("ciseaux"));
+well.addEventListener("click", () => runGame("puit"));
